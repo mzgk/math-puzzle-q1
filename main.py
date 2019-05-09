e0841590c5bda3b5681e38a8cbc331d95c193e9b
tmizugaki <takeshi.mizukaki@unisys.co.jp>
2019-05-09 17:24:03 +0900

    mod: 基数変換およびチェック処理を、for-elseを使い1行にまとめる

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ def main():
     i = 9
     while True:
         i += 2
-        if format(i, 'd') != format(i, 'd')[::-1]:
-            continue
-        if format(i, 'b') != format(i, 'b')[::-1]:
-            continue
-        if format(i, 'o') != format(i, 'o')[::-1]:
-            continue
-        print("Result-> 10:{}, 2:{}, 8:{}".format(format(i, 'd'), format(i, 'b'), format(i, 'o')))
-        break
+        for radix in ['d', 'b', 'o']:
+            if format(i, radix) != format(i, radix)[::-1]:
+                break
+        else:
+            print("Result-> 10:{}, 2:{}, 8:{}".format(format(i, 'd'), format(i, 'b'), format(i, 'o')))
+            break   # while終了
     t2 = time.time()
     print('Elapse-> {}'.format(t2 - t1))
 
