729cb2bc0d82b7aa44eea6b25dfcac4c387b4e8f
tmizugaki <takeshi.mizukaki@unisys.co.jp>
2019-05-09 17:10:51 +0900

    mod: 複数個所の+=2を1回にまとめる

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ import time
 
 def main():
     t1 = time.time()
-    i = 11
+    i = 9
     while True:
+        i += 2
         if format(i, 'd') != format(i, 'd')[::-1]:
-            i += 2
             continue
         if format(i, 'b') != format(i, 'b')[::-1]:
-            i += 2
             continue
         if format(i, 'o') != format(i, 'o')[::-1]:
-            i += 2
             continue
         print("Result-> 10:{}, 2:{}, 8:{}".format(format(i, 'd'), format(i, 'b'), format(i, 'o')))
         break
