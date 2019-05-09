281bc9316df5a67cac07cef26cafdc0b4a4b5fd3
tmizugaki <takeshi.mizukaki@unisys.co.jp>
2019-05-09 17:31:08 +0900

    refactor: コメント追加

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import time
 
 def main():
     t1 = time.time()
+
     i = 9
     while True:
         i += 2
         for radix in ['d', 'b', 'o']:
             if format(i, radix) != format(i, radix)[::-1]:
                 break
-        else:
+        else:   # forループ正常終了時の処理
             print("Result-> 10:{}, 2:{}, 8:{}".format(format(i, 'd'), format(i, 'b'), format(i, 'o')))
             break   # while終了
+
     t2 = time.time()
     print('Elapse-> {}'.format(t2 - t1))
 
